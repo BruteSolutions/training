6a73c0545a0142493eb7efd3dbe51d0eb48fb79c
Petter Salminen <kemikaze@gmail.com>
2012-02-01 11:12:32 +0100

    Update truck.cpp

--- truck.cpp
+++ truck.cpp
@@ -3,10 +3,15 @@ class Truck : public Vehicle, public Autonomous {
  public:
   Truck(std::string a, int b){
     Truck::set_values(a,b);
+    float Truck::getPrice();
   };
 
   void set_values(std::string a,int b){
     brand = a;
     year = b;
   };
+  
+  float getPrice(){
+      return 0; // do magic
+  }
  
