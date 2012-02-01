f9619531e6884f1bdfb30de983cc49618b47649e
Petter Salminen <kemikaze@gmail.com>
2012-02-01 11:09:12 +0100

    Instead of #include copy pasta remove .h file.

--- truck.cpp
+++ truck.cpp
@@ -1,4 +1,12 @@
-#include <truck.h>
+class Truck : public Vehicle, public Autonomous {
+    
+ public:
+  Truck(std::string a, int b){
+    Truck::set_values(a,b);
+  };
 
-
-
+  void set_values(std::string a,int b){
+    brand = a;
+    year = b;
+  };
+ 
